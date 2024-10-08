73cea99ac1636a291d5b0291804aee73e876caaa
kqueuey <kristinaqualben@gmail.com>
2024-10-08 15:49:15 -0400

    save

--- pytorch_model.py
+++ pytorch_model.py
@@ -1,19 +1,21 @@
 import torch
 import torch.nn as nn
 import torchvision
-from torchvision.datasets import OxfordIIITPet
+#from torchvision.datasets import OxfordIIITPet
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchmetrics import Precision, Recall
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+#from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
+import tensorflow_datasets as tfds
 
 from torchvision.transforms import v2
 from torchvision.io import read_image
 
 plt.rcParams["savefig.bbox"] = 'tight'
-PIC_PATH ='/Users/kristinaqualben/Desktop/Fun/puppy_pics/data/oxford-pets'
+DIR ='/Volumes/KCQDrive/projects'
+DATASET = 'stanford_dogs'
 torch.manual_seed(1)
 
 class Net(nn.Module):
@@ -56,50 +58,17 @@ class preprocessDataset(torch.utils.data.Dataset):
 class Bowzer():
     def __init__(self, resize_n:int = 64):
         self.resize_n = resize_n
-        weights = torchvision.models.resnet.ResNet34_Weights.DEFAULT
-        self.raw_train_data = OxfordIIITPet(root=PIC_PATH, target_types='category', download=True)
-        self.raw_test_data = OxfordIIITPet(root=PIC_PATH,target_types='category',download=True,split="test")
-
-        # self.train_transforms = transforms.Compose([
-        #     transforms.RandomHorizontalFlip(),
-        #     transforms.RandomRotation(45),
-        #     transforms.RandomAutocontrast(),
-        #     transforms.ToTensor(),
-        #     transforms.Resize((self.resize_n,self.resize_n)),
-        #     transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-        #     ])
-        # self.test_transforms = transforms.Compose([
-        #     transforms.ToTensor(),
-        #     transforms.Resize((self.resize_n,self.resize_n)),
-        #     transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-        #     ])
+        weights = torchvision.models.resnet.ResNet50_Weights.DEFAULT
+   
+        self.raw_train_data, self.train_info = tfds.load(DATASET, split='train', shuffle_files=True, data_dir = f"{DIR}/tensorflow_datasets/", with_info=True)
+        self.raw_test_data, self.test_info = tfds.load(DATASET, split='test', shuffle_files=True, data_dir = f"{DIR}/tensorflow_datasets/", with_info=True)
 
         self.train_data = preprocessDataset(self.raw_train_data,  weights.transforms())
         self.test_data = preprocessDataset(self.raw_test_data,  weights.transforms())
-        self.train_data, self.val_data = train_test_split(self.train_data, test_size=0.2, random_state=0)
+        #self.train_data, self.val_data = train_test_split(self.train_data, test_size=0.2, random_state=0)
         self.dataloader_train = DataLoader(self.train_data, shuffle=True, batch_size=64)
-        self.dataloader_val = DataLoader(self.val_data, shuffle=True, batch_size=64)
+        #self.dataloader_val = DataLoader(self.val_data, shuffle=True, batch_size=64)
         self.dataloader_test = DataLoader(self.test_data, shuffle=True, batch_size=64)
-
-    def create_grid(self, raw_train_data, train_data):
-        raw_sample = []
-        train_sample = []
-
-        rw = iter(raw_train_data)
-        tr = iter(train_data)
-
-        for i in range(16):
-            raw_sample.append(next(rw)[0])
-            train_sample.append(next(tr)[0])
-
-        grid1 = torchvision.utils.make_grid([transforms.Resize((224,224),antialias=True)(transforms.ToTensor()(i)) for i in raw_sample], nrow=8)
-        grid2 = torchvision.utils.make_grid([i for i in train_sample], nrow=8)
-        return grid1, grid2
-
-    @staticmethod 
-    def view_grid(grid):
-        return transforms.ToPILImage()(grid)
-        
 
     def train(self, epochs:int=3):
         #Define the model
