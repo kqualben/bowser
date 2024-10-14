f4ce39418490448f526502cfd36fa676c70f15d8
kqueuey <kristinaqualben@gmail.com>
2024-10-14 14:06:35 -0400

    save

--- pytorch_model.py
+++ pytorch_model.py
@@ -12,6 +12,8 @@ import tensorflow_datasets as tfds
 from tqdm import tqdm
 from torchvision.transforms import v2
 from torchvision.io import read_image
+from PIL import Image
+from typing import Dict, Literal
 
 plt.rcParams["savefig.bbox"] = 'tight'
 DIR ='/Volumes/KCQDrive/projects'
@@ -23,16 +25,23 @@ class Net(nn.Module):
         super().__init__()
         # Define feature extractor
         self.feature_extractor = nn.Sequential(
-           nn.Conv2d(3, resize_n/2, kernel_size=3, padding=1),
+            #input image: 3 * resize_n * resize_n
+           nn.Conv2d(3, resize_n//2, kernel_size=3, padding=1), #resize/2 output filters
+           #resize_n/2 * resize_n * resize_n
            nn.ELU(),
-           nn.MaxPool2d(kernel_size=2),
-           nn.Conv2d(resize_n/2, resize_n, kernel_size=3, padding=1),
+           nn.MaxPool2d(kernel_size=2), #cut h and w in half
+           #resize_n/2 * resize_n/2 * resize_n/2
+           nn.Conv2d(resize_n//2, resize_n, kernel_size=3, padding=1), #resize_n output filters
+           #resize_n * resize_n/2 * resize_n/2
            nn.ELU(),
-           nn.MaxPool2d(kernel_size=2),
+           nn.MaxPool2d(kernel_size=2), #cut h and w in half
+           #resize_n * (resize_n/2)/2 * (resize_n/2)/2
+           #resize_n * resize_n/4 * resize_n/4
            nn.Flatten(),
            )
         # Define classifier
-        self.classifier = nn.Linear(resize_n**2, num_classes)
+        #resize_n * resize_n/4 * resize_n/4
+        self.classifier = nn.Linear(resize_n*(resize_n//4)*(resize_n//4), num_classes)
 
     def forward(self, x):
         x = self.feature_extractor(x)
@@ -55,9 +64,8 @@ class preprocessDataset(torch.utils.data.Dataset):
     
 
 class Bowzer():
-    def __init__(self, resize_n:int = 64, num_classes:int=37):
+    def __init__(self, resize_n:int = 64):
         self.resize_n = resize_n
-        self.num_classes = num_classes
         self.train_transforms = (
             transforms
             .Compose([
@@ -75,10 +83,12 @@ class Bowzer():
                 transforms.Resize((self.resize_n,self.resize_n))
             ])
         )
-        #weights = torchvision.models.resnet.ResNet50_Weights.DEFAULT
-
         self.train_data = OxfordIIITPet(root=DIR, download=True, transform=self.train_transforms)
         self.test_data = OxfordIIITPet(root=DIR, download=True,split="test", transform=self.test_transforms)
+        
+        self.num_classes = len(set(self.train_data._labels))
+        
+        #weights = torchvision.models.resnet.ResNet50_Weights.DEFAULT
         #self.raw_train_data, self.train_info = tfds.load(DATASET, split='train', shuffle_files=True, data_dir = f"{DIR}/tensorflow_datasets/", with_info=True)
         #self.raw_test_data, self.test_info = tfds.load(DATASET, split='test', shuffle_files=True, data_dir = f"{DIR}/tensorflow_datasets/", with_info=True)
 
@@ -95,9 +105,14 @@ class Bowzer():
         #self.train_data = preprocessDataset(self.raw_train_data,  weights.transforms())
         #self.test_data = preprocessDataset(self.raw_test_data,  weights.transforms())
         #self.train_data, self.val_data = train_test_split(self.train_data, test_size=0.2, random_state=0)
-        self.dataloader_train = DataLoader(self.train_data, shuffle=True, batch_size=64)
+        self.dataloader_train = DataLoader(self.train_data, shuffle=True, batch_size=self.num_classes)
         #self.dataloader_val = DataLoader(self.val_data, shuffle=True, batch_size=64)
-        self.dataloader_test = DataLoader(self.test_data, shuffle=True, batch_size=64)
+        self.dataloader_test = DataLoader(self.test_data, shuffle=True, batch_size=self.num_classes)
+
+    
+    def show_img(self, image, transform):
+        plt.imshow(transform(Image.open(image)).squeeze(0).permute(1,2,0))
+        plt.show()
 
     def train(self, epochs:int=3):
         #Define the model
@@ -119,25 +134,26 @@ class Bowzer():
             epoch_loss = self.running_loss / len(self.dataloader_train)
             print(f"Epoch {epoch+1}, Loss: {epoch_loss:.4f}")
 
-    def evaluate(self):
-        metric_precision = Precision(
-            task='multiclass',
-            num_classes=self.train_info.features["label"].num_classes,
-            average='macro')
-        metric_recall = Recall(
-            task='multiclass',
-            num_classes=self.train_info.features["label"].num_classes,
-            average='macro')
-        self.net.eval()
-        with torch.no_grad():
-            for images, labels in self.dataloader_test:
-                outputs = self.net(images)
-                _, preds = torch.max(outputs, 1)
-                metric_precision(preds, labels)
-                metric_recall(preds, labels)
-            self.precision = metric_precision.compute()
-            self.recall = metric_recall.compute()
-            print(f"Precision: {self.precision}")
-            print(f"Recall: {self.recall}")
+def evaluate(model, num_classes, dataloader_test, average: Literal['macro','micro','weighted',None]):
+    metric_precision = Precision(
+        task='multiclass',
+        num_classes=num_classes,
+        average=average)
+    metric_recall = Recall(
+        task='multiclass',
+        num_classes=num_classes,
+        average=average)
+    model.eval()
+    with torch.no_grad():
+        for images, labels in dataloader_test:
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+            metric_precision(preds, labels)
+            metric_recall(preds, labels)
+    precision = metric_precision.compute()
+    recall = metric_recall.compute()
+    print(f"Precision: {precision}")
+    print(f"Recall: {recall}")
+    return precision, recall
 
             
