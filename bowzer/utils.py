04b413488af08390de740b6abcd9e2da1b6c774e
kqueuey <kristinaqualben@gmail.com>
2024-10-21 21:17:21 -0400

    add save_json and open_json

--- bowzer/utils.py
+++ bowzer/utils.py
@@ -1,4 +1,18 @@
 from PIL import Image
+import json
+import os
 
 def open_image(image_path:str) -> Image:
     return Image.open(image_path)
+
+def save_json(data: dict, directory: str, filename: str) -> None:
+    location = os.path.join(directory, filename)
+    with open(location, 'w') as f:
+        json.dump(data, f)
+    print(f"File saved to: {location}")
+
+def open_json(path: str) -> dict:
+    print(f"Loading: {path}")
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
