e473f7fb96033cb0c61f3adda6c2b90b8bf2b5d1
kqueuey <kristinaqualben@gmail.com>
2024-10-25 15:24:42 -0400

    add viz

--- bowzer/utils.py
+++ bowzer/utils.py
@@ -1,9 +1,11 @@
 import json
 import logging
 import os
-from typing import Dict
+from typing import Dict, Literal
 
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def open_image(image_path: str) -> Image:
@@ -61,3 +63,116 @@ def get_target_image_dict(root: str = "./images/targets/") -> Dict[str, dict]:
             for image in os.listdir(f"{root}/{folder}")
         }
     return image_dict
+
+
+def compare_model_loss(model_dict: dict, loss: Literal["train", "val"] = "train"):
+    loss_target = "avg_loss" if loss == "train" else "avg_val_loss"
+    model_avg_losses = {}
+    for label in model_dict:
+        model_perf = open_json(f"{model_dict[label]}_performance.json")
+        epoch_avg_loss = []
+        for epoch in [x for x in model_perf.keys() if "epoch" in x.lower()]:
+            epoch_avg_loss.append(model_perf[epoch][loss_target])
+        model_avg_losses[label] = {
+            "loss": epoch_avg_loss,
+            "accuracy": model_perf["accuracy"] * 100,
+        }
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    for label in model_avg_losses:
+        total_loss = model_avg_losses[label]["loss"]
+        acc = model_avg_losses[label]["accuracy"]
+        epoch_range = range(1, len(total_loss) + 1)
+        plt.scatter(x=epoch_range, y=total_loss, label=f"{label}")
+        ax.annotate(
+            f"Accuracy:\n{acc:.2f}%",
+            xy=(epoch_range[-1], total_loss[-1]),
+            xytext=(0, 25),
+            textcoords="offset points",
+            arrowprops=dict(facecolor="black", arrowstyle="->"),
+            horizontalalignment="center",
+            verticalalignment="bottom",
+        )
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel(f"{loss.title()} Loss")
+    plt.legend()
+    plt.title(f"Bowser Model {loss.title()} Loss Comparison")
+    fig.tight_layout()
+    plt.show()
+
+
+def view_model_performance(
+    model_name: str,
+    model_performance_path: str,
+    save_fig: bool = False,
+    show_batch_losses: bool = False,
+) -> None:
+    subplots = 1
+    plot_batch_losses = False
+    model_perf = open_json(model_performance_path)
+    epoch_keys = [x for x in model_perf.keys() if "epoch" in x.lower()]
+    title = f"{model_name}\nPrecision: {model_perf['precision']:.2f} Recall: {model_perf['recall']:.2f}"
+    if (
+        "loss_list" in model_perf[epoch_keys[0]].keys()
+        and show_batch_losses is not False
+    ):
+        plot_batch_losses = True
+        subplots += 2
+
+    accuracy = 100 * model_perf["accuracy"]
+    epoch_avg_loss = []
+    epoch_val_loss = []
+    fig, ax = plt.subplots(1, subplots, figsize=(6 * (subplots), 6))
+    for idx, epoch in enumerate(epoch_keys):
+        epoch_avg_loss.append(model_perf[epoch]["avg_loss"])
+        epoch_val_loss.append(model_perf[epoch]["avg_val_loss"])
+        if plot_batch_losses:
+            epoch_losses = model_perf[epoch]["loss_list"]
+            ax[1].scatter(
+                x=np.linspace(1, len(epoch_losses), len(epoch_losses)).astype(int),
+                y=epoch_losses,
+                label=epoch,
+                marker=["o" if idx < 20 else "*" if idx >= 20 and idx < 40 else "s"][0],
+            )
+            ax[2].hist(
+                epoch_losses,
+                alpha=np.linspace(0.25, 0.75, len(epoch_keys)).astype(float)[idx],
+                label=epoch,
+            )
+    epoch_range = range(1, len(epoch_avg_loss) + 1)
+    if plot_batch_losses:
+        ax_ph = ax[0]
+    else:
+        ax_ph = ax
+    ax_ph.scatter(x=epoch_range, y=epoch_avg_loss, label="Train Loss")
+    ax_ph.scatter(x=epoch_range, y=epoch_val_loss, label="Val loss", color="red")
+    ax_ph.set_title(f"Accuracy: {accuracy:.2f}%")
+    ax_ph.legend()
+    ax_ph.set_xlabel("Epoch")
+    ax_ph.set_ylabel("Loss")
+    if plot_batch_losses:
+        ax[1].set_title("Batch Loss")
+        ax[2].set_title("Loss Distribution")
+        ax[1].set_xlabel("Batch")
+        ax[2].set_ylabel("Loss")
+        ax[2].set_xlabel("Loss")
+        ax[2].set_ylabel("Frequency")
+        lines, labels = (
+            ax[1].get_legend_handles_labels()[0],
+            ax[1].get_legend_handles_labels()[1],
+        )
+        fig.legend(
+            lines,
+            labels,
+            loc="upper left",
+            bbox_to_anchor=(1, 0.86),
+            ncol=(round(len(epoch_keys) ** (1 / 4)) if len(epoch_keys) > 10 else 1),
+        )
+
+    fig.suptitle(title, size="large", weight="bold")
+    fig.tight_layout()
+    if save_fig:
+        path = model_performance_path.replace(".json", "_plot.jpg")
+        plt.savefig(path)
+        print(f"Plot saved to: {path}")
+    plt.show()
