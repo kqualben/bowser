1137594d025cbeecd08bad4149b839df7960b29d
kqueuey <kristinaqualben@gmail.com>
2024-10-30 20:53:43 -0400

    include cats bool

--- bowzer/config.py
+++ bowzer/config.py
@@ -14,6 +14,7 @@ class ModelSettings:
     learning_rate: float
     batch_size: int
     resize_n: int
+    include_cats: bool
     train_transform: v2.Compose
     test_transform: v2.Compose
 
@@ -28,6 +29,7 @@ class ModelSettings:
             learning_rate=config["learning_rate"],
             batch_size=config["batch_size"],
             resize_n=config["resize_n"],
+            include_cats=config["include_cats"],
             train_transform=config["train_transform"],
             test_transform=config["test_transform"],
         )
