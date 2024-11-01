3abf5d26b1215afe530a86ec55bece7611c10c0c
kqueuey <kristinaqualben@gmail.com>
2024-11-01 16:50:27 -0400

    Update model_train_configs.py

--- model_train_configs.py
+++ model_train_configs.py
@@ -5,7 +5,7 @@ from torchvision.transforms import v2 as transforms
 config_0_base = {
     "batch_size": 64,
     "resize_n": 128,
-    "learning_rate": 0.001,
+    "learning_rate": 0.00001,
     "include_cats": False,
 }
 config_0_transforms = {
@@ -39,10 +39,11 @@ config_0_e50 = {"info": "First Trial. Trained with 50 Epochs", "epochs": 50}
 config_0_e50.update(config_0_base)
 config_0_e50.update(config_0_transforms)
 
+
 #### Retrain to Adjust for Overfitting ###
 prod_config_base = {
-    "batch_size": 128,
-    "resize_n": 192,
+    "batch_size": 16,
+    "resize_n": 128,
     "learning_rate": 0.00001,
     "include_cats": False,
 }
@@ -50,12 +51,12 @@ prod_config_transforms = {
     "train_transform": transforms.Compose(
         [
             transforms.ToDtype(torch.uint8, scale=True),
-            transforms.RandomResizedCrop(prod_config_base["resize_n"], antialias=True),
-            transforms.RandomAutocontrast(),
+            transforms.Resize(
+                (config_0_base["resize_n"], config_0_base["resize_n"]), antialias=True
+            ),
+            transforms.RandomGrayscale(),
             transforms.RandomHorizontalFlip(),
-            # transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
             transforms.RandomRotation(45),
-            # Because ToTensor() is deprecated:
             transforms.ToImage(),
             transforms.ToDtype(torch.float32, scale=True),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -64,8 +65,9 @@ prod_config_transforms = {
     "test_transform": transforms.Compose(
         [
             transforms.ToDtype(torch.uint8, scale=True),
-            transforms.RandomResizedCrop(prod_config_base["resize_n"], antialias=True),
-            # Because ToTensor() is deprecated:
+            transforms.Resize(
+                (config_0_base["resize_n"], config_0_base["resize_n"]), antialias=True
+            ),
             transforms.ToImage(),
             transforms.ToDtype(torch.float32, scale=True),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -73,9 +75,50 @@ prod_config_transforms = {
     ),
 }
 
+
 prod_config = {
     "info": "Final Model Config.",
     "epochs": 50,
 }
 prod_config.update(prod_config_base)
 prod_config.update(prod_config_transforms)
+
+# prod_config_base = {
+#     "batch_size": 128,
+#     "resize_n": 192,
+#     "learning_rate": 0.00001,
+#     "include_cats": False,
+# }
+# prod_config_transforms = {
+#     "train_transform": transforms.Compose(
+#         [
+#             transforms.ToDtype(torch.uint8, scale=True),
+#             transforms.RandomResizedCrop(prod_config_base["resize_n"], antialias=True),
+#             transforms.RandomAutocontrast(),
+#             transforms.RandomHorizontalFlip(),
+#             # transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
+#             transforms.RandomRotation(45),
+#             # Because ToTensor() is deprecated:
+#             transforms.ToImage(),
+#             transforms.ToDtype(torch.float32, scale=True),
+#             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#         ]
+#     ),
+#     "test_transform": transforms.Compose(
+#         [
+#             transforms.ToDtype(torch.uint8, scale=True),
+#             transforms.RandomResizedCrop(prod_config_base["resize_n"], antialias=True),
+#             # Because ToTensor() is deprecated:
+#             transforms.ToImage(),
+#             transforms.ToDtype(torch.float32, scale=True),
+#             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#         ]
+#     ),
+# }
+
+# prod_config = {
+#     "info": "Final Model Config.",
+#     "epochs": 50,
+# }
+# prod_config.update(prod_config_base)
+# prod_config.update(prod_config_transforms)
