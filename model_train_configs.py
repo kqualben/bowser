cd164454681345e70e059023a7d5ab0af73b7b02
kqueuey <kristinaqualben@gmail.com>
2024-11-03 17:03:13 -0500

    Update model_train_configs.py

--- model_train_configs.py
+++ model_train_configs.py
@@ -89,13 +89,3 @@ prod_config = {
     "resize_n": prod_config_resize_n,
 }
 prod_config.update(prod_config_transforms)
-
-prod_config_with_cats = {
-    "info": "Final Model Config.",
-    "epochs": 50,
-    "learning_rate": 0.00001,
-    "include_cats": True,
-    "batch_size": 32,
-    "resize_n": prod_config_resize_n,
-}
-prod_config_with_cats.update(prod_config_transforms)
